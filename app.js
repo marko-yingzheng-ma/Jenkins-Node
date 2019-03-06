adf7073614ff0732d804725022db1a620b34b8ce
Yingzheng Ma <yma39@ford.com>
2019-03-06 15:33:08 -0800

    routes

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+  res.send('Hello Express');
+});
+
+app.get('/api', function(req, res) {
+  res.json({ name: 'Marko', age: 25 });
+});
+
+app.get('/person/:name', function(req, res) {
+  const name = req.params['name'];
+  res.send(`Hello ${name}`);
+});
+
+app.listen(port);
