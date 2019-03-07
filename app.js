8521c39e84e218807c921dc63977bb03803260dd
Yingzheng Ma <yma39@ford.com>
2019-03-06 16:24:44 -0800

    Middleware

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.use('/', function(req, res, next) {
+  console.log(`Request Url: ${ req.url }`);
+  next();
+});
+
 app.get('/', function(req, res) {
-  res.send('Hello Express');
+  res.send('<html><head><link href=assets/styles.css type=text/css rel=stylesheet /></head><body><h1>Hello World!</h1></body></html>');
 });
 
 app.get('/api', function(req, res) {
