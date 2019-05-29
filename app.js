f1f68d839c10ac2568c1ad5e3c4eecf706931362
Yingzheng Ma <yma39@ford.com>
2019-05-29 14:10:39 -0700

    for jenkins

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
-
-app.use('/assets', express.static(__dirname + '/public'));
-
-app.use('/', function(req, res, next) {
-  console.log(`Request Url: ${ req.url }`);
-  next();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-app.get('/', function(req, res) {
-  res.send('<html><head><link href=assets/styles.css type=text/css rel=stylesheet /></head><body><h1>Hello World!</h1></body></html>');
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
-
-app.get('/api', function(req, res) {
-  res.json({ name: 'Marko', age: 25 });
-});
-
-app.get('/person/:name', function(req, res) {
-  const name = req.params['name'];
-  res.send(`Hello ${name}`);
-});
-
-app.listen(port);
